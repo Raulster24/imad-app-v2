e20fbece5f31c53203475ec1f1e354706d73a02d
Rahul Srivastava <rahulsri.pccs@gmail.com>
2017-02-22 21:58:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ var htmltemplate = `
 				${title}
 			</title>
 			<link href="/ui/style.css" rel="stylesheet" />
+			<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 			<meta name = "viewport" content="width=device-width, initial-scale=1"/>
 		 </head>
 		 
