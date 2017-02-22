ff1aec27556b2b3cec69c71c34fbad618fce7e9e
Rahul Srivastava <rahulsri.pccs@gmail.com>
2017-02-22 23:30:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick = function(){
 
 	};
 
-	request.open('GET','http://localhost:8080/counter',true);
+	request.open('GET','http://raulster24.imad.hasura-app.io/counter',true);
 	request.send(null);
 };
 
