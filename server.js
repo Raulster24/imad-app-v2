3dd65a8f495cfdc2f694232bb37953bade8de6d5
Rahul Srivastava <rahulsri.pccs@gmail.com>
2017-03-03 12:08:20 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -106,6 +106,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi1.png'));
+});
+
 var counter = 0;
 app.get('/counter', function(req,res){
 	counter = counter + 1;
@@ -148,6 +152,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+
+
  app.get('/ui/madi1.png', function(req, res){
 	 res.sendFile(path.join(__dirname, 'ui', 'madi1.png'));
 	});
