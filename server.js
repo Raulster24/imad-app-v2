3a04e06563395d15f79e8084118fcdba87c45af5
Rahul Srivastava <rahulsri.pccs@gmail.com>
2017-03-09 17:05:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
+var crypto = require('crypto');
 app.use(morgan('combined'));
 
 var config = {
@@ -72,6 +73,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input, salt){
+    
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req,res){
+    
+    var hashedString = hash(req.params.input, 'this is some random string');
+    res.send(hashedString);
+    
+});
 
 app.get('/test-db', function(req,res) {
     
