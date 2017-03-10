e0b01dad5f9228c54878b3110905f4e3a2935c05
Rahul Srivastava <rahulsri.pccs@gmail.com>
2017-03-10 15:20:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,11 @@ app.get('/check-login', function(req,res){
     
 });
 
-
+app.get('/logout', function(req,res){
+    
+    delete req.session.auth;
+    res.send('logged out');
+});
 
 
 app.get('/test-db', function(req,res) {
